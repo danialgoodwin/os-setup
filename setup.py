b67c4e22385d0a6e7e60d1b996b87f24370c80e8
Danial Goodwin <danialgoodwin@gmail.com>
2020-03-16 12:45:11 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+
+def setup_linux():
+    print('setup_linux()')
+
+
+def setup_mac():
+    print('setup_mac()')
+    # TODO: Spectacle
+
+
+def setup_windows():
+    print('setup_windows()')
+    # TODO: Notepad++?
+
+
+def setup():
+    print('setup()')
+
+def main():
+    print('main()')
+
+
+if __name__ = '__main__':
+    main()
